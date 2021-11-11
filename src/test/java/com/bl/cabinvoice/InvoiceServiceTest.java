3fd4408b4e434c50bc46d1ecf1163766c51d349a
unknown <altamashbagwan1@gmail.com>
2021-11-12 00:37:49 +0530

    Added: Fuctioning of Multiple Fare

--- src/test/java/com/bl/cabinvoice/InvoiceServiceTest.java
+++ src/test/java/com/bl/cabinvoice/InvoiceServiceTest.java
@@ -5,11 +5,30 @@ import org.junit.Test;
 
 public class InvoiceServiceTest {
     @Test
-    public void givenDistanceAndTime_ShouldReturnTotalFare(){
-        InvoiceGenerator invoicegenerator =  new InvoiceGenerator();
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
-        double fare =  invoicegenerator.calculateFare(distance, time);
-        Assert.assertEquals(25.0,fare, 0.0);
+        double fare = invoiceGenerator.CalculateFare(distance, time);
+        Assert.assertEquals(25, fare, 0.0);
+    }
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnMinimumFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.CalculateFare(distance, time);
+        Assert.assertEquals(5, fare, 0.0);
+    }
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnMultipleRides() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        double fare = invoiceGenerator.CalculateFare(rides);
+        Assert.assertEquals(30, fare, 0.0);
     }
 }
