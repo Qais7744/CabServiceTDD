9cb60028d468e700a86f6af2281de63c567bd952
unknown <altamashbagwan1@gmail.com>
2021-11-12 00:47:44 +0530

    Added: Fuctioning of Enhanced Invoice

--- src/test/java/com/bl/cabinvoice/InvoiceServiceTest.java
+++ src/test/java/com/bl/cabinvoice/InvoiceServiceTest.java
@@ -1,34 +1,39 @@
 package com.bl.cabinvoice;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class InvoiceServiceTest {
+    InvoiceGenerator invoiceGenerator = null;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
-        double fare = invoiceGenerator.CalculateFare(distance, time);
+        double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(25, fare, 0.0);
     }
 
     @Test
     public void givenDistanceAndTime_ShouldReturnMinimumFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 0.1;
         int time = 1;
-        double fare = invoiceGenerator.CalculateFare(distance, time);
+        double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(5, fare, 0.0);
     }
 
     @Test
-    public void givenDistanceAndTime_ShouldReturnMultipleRides() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+    public void givenDistanceAndTime_ShouldReturnInvoiceSummary() {
         Ride[] rides = {new Ride(2.0, 5),
                 new Ride(0.1, 1)
         };
-        double fare = invoiceGenerator.CalculateFare(rides);
-        Assert.assertEquals(30, fare, 0.0);
+        InvoiceSummary summary = InvoiceGenerator.calculateFare(rides);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
+        Assert.assertEquals(expectedInvoiceSummary, summary);
     }
 }
